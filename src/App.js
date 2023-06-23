0c613717521cfd05e52654cfc4a605dc85d61f61
Yuqiu Ge <yuqiu.ge@gmail.com>
2023-06-23 15:54:32 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Button from "./components/Button";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button />
+    </div>
+  );
+}
